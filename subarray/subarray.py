76c5194e521cd086d1adba7253d0ea91aced5b8e
Priyanka Parikh <priyanka.parikh6@gmail.com>
2020-05-18 14:58:42 -0700

    some more subarray problems

--- subarray/subarray.py
+++ subarray/subarray.py
@@ -1,3 +1,5 @@
+import math
+
 def subarray_average(k, arr):
     """Given an array, find the average of all contiguous subarrays of size ‘K’ in it."""
     window_start = 0
@@ -32,3 +34,49 @@ def maximum_sum_subarray(k, arr):
 
     return max_sum
 
+
+def smallest_subarray(k, arr):
+    """Given an array find the smallest subarray that sums up to match a target."""
+    window_start = 0
+    window_sum = 0
+    min_size = math.inf
+
+    for window_end in range(0, len(arr)):
+        window_sum += arr[window_end]
+        while window_sum >= k:
+            min_size = min(min_size, window_end - window_start + 1)
+            window_sum -= arr[window_start]
+            window_start += 1
+    if min_size == math.inf:
+        return 0
+    return  min_size
+
+
+def longest_substr_with_distinct_chars(k ,chars):
+    """Given a substring find the longest substring with k distinct chars."""
+    char_frequency = {}
+    max_len = 0
+    w_start = 0
+
+    for w_end in range(len(chars)):
+        right_char = chars[w_end]
+        if right_char not in char_frequency:
+            char_frequency[right_char] = 0
+        char_frequency[right_char] += 1
+
+        while len(char_frequency) > k:
+            left_char = chars[w_start]
+            char_frequency[left_char] -= 1
+            if char_frequency[left_char] == 0:
+                del char_frequency[left_char]
+            w_start += 1
+            # shrink the window
+            # remember the maximum length so far
+            max_len = max(max_len, w_end - w_start + 1)
+
+    return max_len
+
+
+def longest_substring_no_repeat(str):
+    """Given a string find the longest substring with no repeating character"""
+    pass
--- tests/test_subarray.py
+++ tests/test_subarray.py
@@ -1,5 +1,6 @@
 from subarray.subarray import subarray_average, maximum_sum_subarray
-
+from subarray.subarray import smallest_subarray, longest_substr_with_distinct_chars
+from subarray.subarray import longest_substring_no_repeat
 
 def test_subarray_average():
     """find the average of all contiguous subarrays of size ‘K’ in a given array."""
@@ -11,3 +12,20 @@ def test_maximum_sum_subarray():
     assert maximum_sum_subarray(3, [2, 1, 5, 1, 3, 2]) == 9
     assert maximum_sum_subarray(2, [2, 3, 4, 1, 5]) == 7
 
+
+def test_smallest_subarray():
+    """Given an array find the smallest subarray that sums to a given target"""
+    assert smallest_subarray(7, [2, 1, 5, 2, 3, 2]) == 2
+    assert smallest_subarray(7, [2, 1, 5, 2, 8]) == 1
+    assert smallest_subarray(8, [3, 4, 1, 1, 6]) == 3
+
+
+def test_longest_substr_with_distinct_chars():
+    """Given a substring find the longest substring with k distinct chars."""
+    assert longest_substr_with_distinct_chars(2, "araaci") == 4
+    assert longest_substr_with_distinct_chars(1, "araaci") == 2
+    assert longest_substr_with_distinct_chars(3, "cbbebi") == 5
+
+def test_longest_substr_no_repeat():
+    """Given a string find the longest substring with no repeating character"""
+    pass
